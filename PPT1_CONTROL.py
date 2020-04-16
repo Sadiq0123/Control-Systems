4f499af0b2a9b75389a7de16049b24c3137497ce
Sadiq <47571312+SuperSadiq1234@users.noreply.github.com>
2020-04-16 21:06:45 +0530

    Update PPT1_CONTROL.py

--- PPT1_CONTROL.py
+++ PPT1_CONTROL.py
@@ -1,8 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-omega = np.append(np.linspace(0,20,100),np.linspace(20,500,100))
+# Different values of w for points
+omega = np.append(np.linspace(0,20,100),np.linspace(20,500,100)) 
+
+#Solve the equation into (Magnitude) * exp(Theta)
+
+# This is Theta equation
 theta = np.arctan(omega)-np.arctan(omega/10)
+
+# This is the magnitude equation
 radius = 10*np.sqrt((1+np.square(omega))/(100+np.square(omega)))
 
 plt.plot(radius*np.cos(theta), radius * np.sin(theta))
